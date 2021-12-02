ab6e357d744f6b0deb8537214896345ab33df644
parryk303 <kyle.parry.303@gmail.com>
2021-12-01 20:10:20 -0700

    Add checkPalindrome

---
+++ checkPalindrome.js
@@ -0,0 +1,18 @@
+/*
+Given the string, check if it is a palindrome.
+
+Example
+
+For inputString = "aabaa", the output should be
+solution(inputString) = true;
+For inputString = "abac", the output should be
+solution(inputString) = false;
+For inputString = "a", the output should be
+solution(inputString) = true.
+*/
+function solution(str) {
+    var re = /[\W_]/g;
+    var lowRegStr = str.toLowerCase().replace(re, '');
+    var reverseStr = lowRegStr.split('').reverse().join(''); 
+    return reverseStr === lowRegStr;
+  }
