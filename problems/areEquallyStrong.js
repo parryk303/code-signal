f9cccd9a9d2c2eadabfc8b7289f2fa1b5034d2a0
parryk303 <kyle.parry.303@gmail.com>
2022-01-11 20:43:15 -0700

    Add areEquallyStrong

---
+++ problems/areEquallyStrong.js
@@ -0,0 +1,20 @@
+/*
+Call two arms equally strong if the heaviest weights they each are able to lift are equal.
+
+Call two people equally strong if their strongest arms are equally strong (the strongest arm can be both the right and the left), and so are their weakest arms.
+
+Given your and your friend's arms' lifting capabilities find out if you two are equally strong.
+
+Example
+
+For yourLeft = 10, yourRight = 15, friendsLeft = 15, and friendsRight = 10, the output should be
+solution(yourLeft, yourRight, friendsLeft, friendsRight) = true;
+For yourLeft = 15, yourRight = 10, friendsLeft = 15, and friendsRight = 10, the output should be
+solution(yourLeft, yourRight, friendsLeft, friendsRight) = true;
+For yourLeft = 15, yourRight = 10, friendsLeft = 15, and friendsRight = 9, the output should be
+solution(yourLeft, yourRight, friendsLeft, friendsRight) = false.
+*/
+function solution(yourLeft, yourRight, friendsLeft, friendsRight) {
+    let yl = yourLeft, yr = yourRight, fl = friendsLeft, fr = friendsRight;
+    return (yl == fl && yr == fr || yr == fl && yl == fr);
+}
