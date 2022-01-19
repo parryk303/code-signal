b615fc3ee0823ef853ead1545081837926d74ffc
parryk303 <kyle.parry.303@gmail.com>
2022-01-18 20:37:01 -0700

    Add evenDigitsOnly

--- problems/--template.js
+++ problems/--template.js
@@ -1,3 +1,15 @@
 /*
+Check if all digits of the given integer are even.
 
+Example
+
+For n = 248622, the output should be
+solution(n) = true;
+For n = 642386, the output should be
+solution(n) = false.
 */
+function solution(n) {
+ let nums = [];
+    for(let o of n+"") nums.push(o);
+    return nums.every(v => v % 2 == 0); 
+}
