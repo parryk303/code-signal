fe3a0b5f203d8f331f2abcacd52dee68a0eaa117
parryk303 <kyle.parry.303@gmail.com>
2022-02-21 16:58:04 -0700

    Add sudoku

---
+++ problems/sudoku.js
@@ -0,0 +1,68 @@
+/*
+Sudoku is a number-placement puzzle. The objective is to fill a 9 × 9 grid with digits so that each column, each row, and each of the nine 3 × 3 sub-grids that compose the grid contains all of the digits from 1 to 9.
+
+This algorithm should check if the given grid of numbers represents a correct solution to Sudoku.
+
+Example
+
+For
+grid = [[1, 3, 2, 5, 4, 6, 9, 8, 7],
+        [4, 6, 5, 8, 7, 9, 3, 2, 1],
+        [7, 9, 8, 2, 1, 3, 6, 5, 4],
+        [9, 2, 1, 4, 3, 5, 8, 7, 6],
+        [3, 5, 4, 7, 6, 8, 2, 1, 9],
+        [6, 8, 7, 1, 9, 2, 5, 4, 3],
+        [5, 7, 6, 9, 8, 1, 4, 3, 2],
+        [2, 4, 3, 6, 5, 7, 1, 9, 8],
+        [8, 1, 9, 3, 2, 4, 7, 6, 5]]
+the output should be
+solution(grid) = true;
+
+For
+grid = [[1, 3, 2, 5, 4, 6, 9, 2, 7],
+        [4, 6, 5, 8, 7, 9, 3, 8, 1],
+        [7, 9, 8, 2, 1, 3, 6, 5, 4],
+        [9, 2, 1, 4, 3, 5, 8, 7, 6],
+        [3, 5, 4, 7, 6, 8, 2, 1, 9],
+        [6, 8, 7, 1, 9, 2, 5, 4, 3],
+        [5, 7, 6, 9, 8, 1, 4, 3, 2],
+        [2, 4, 3, 6, 5, 7, 1, 9, 8],
+        [8, 1, 9, 3, 2, 4, 7, 6, 5]]
+the output should be
+solution(grid) = false.
+
+The output should be false: each of the nine 3 × 3 sub-grids should contain all of the digits from 1 to 9.
+These examples are represented on the image below.
+
+
+
+
+*/
+
+function solution(grid) {
+    let n = grid.length, sqrt = Math.sqrt(n);
+      let row = new Array(9), col = new Array(9), box = new Array(9);
+  
+      for(let i = 0; i < n; i++) {
+          
+          for(let j = 0; j < n; j++) {
+              row[grid[i][j] - 1]++;
+              col[grid[j][i] - 1]++;   
+          }
+          
+          let sq = i - i % sqrt;
+          for(let y = sq; y < sq + sqrt; y++) {
+              for(let x = sq; x < sq + sqrt; x++) box[grid[y][x] - 1]++;
+          }
+          
+          for(let j = 0; j < n; j++)
+              if(row[j] == 0 || col[j] == 0 || box[j] == 0) return false;
+          
+          box.fill(0);
+          row.fill(0);
+          col.fill(0);
+      }
+  
+      return true;
+  }
+  
