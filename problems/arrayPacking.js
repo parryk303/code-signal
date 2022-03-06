891aa5541ae22945fafae733e933cb159c30b15a
parryk303 <kyle.parry.303@gmail.com>
2022-03-06 08:44:33 -0700

    Add arrayPacking

---
+++ problems/arrayPacking.js
@@ -0,0 +1,26 @@
+/*
+You are given an array of up to four non-negative integers, each less than 256.
+
+Your task is to pack these integers into one number M in the following way:
+
+The first element of the array occupies the first 8 bits of M;
+The second element occupies next 8 bits, and so on.
+Return the obtained integer M.
+
+Note: the phrase "first bits of M" refers to the least significant bits of M - the right-most bits of an integer. For further clarification see the following example.
+
+Example
+
+For a = [24, 85, 0], the output should be
+solution(a) = 21784.
+
+An array [24, 85, 0] looks like [00011000, 01010101, 00000000] in binary.
+After packing these into one number we get 00000000 01010101 00011000 (spaces are placed for convenience), which equals to 21784.
+*/
+
+function solution(a) {
+    while (a.length < 4)
+            a.push(0);
+        return a[3] << 24 | a[2] << 16 | a[1] << 8 | a[0];
+    }
+    
